83634460ff8a465ae88c1bf29e4c2ada0bb6b11d
Kamente <justinm48000@gmail.com>
2023-06-30 21:45:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
+/* 
+  time complexity = O(n2)
+*/
+
+/* 
+  - create loop to iterate over the array and select each element
+  - create another for loop to iterate over the array but this selects the element before the first loop selects 
+  - create an if condition for when the array pair sum is equal to the target to return true
+  or else return false
+*/
+
+
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); //should return true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); //should return true
+console.log(hasTargetSum([1, 2, 5], 4)); //should return false
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
 
 module.exports = hasTargetSum;
+
